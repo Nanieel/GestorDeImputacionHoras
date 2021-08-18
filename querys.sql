4687a7a58f8b670e042cf19bb9cff212d68c82b7
Daniel Salazar <86621539+Nanieel@users.noreply.github.com>
2021-08-18 14:33:51 +0200

    Update querys.sql

--- querys.sql
+++ querys.sql
@@ -11,3 +11,25 @@ CREATE TABLE users (
 
 INSERT INTO users (email, password, name, last_name)
 VALUES ('admin', 'admin', 'Admin' ,'Admin'); 
+
+
+
+
+CREATE TABLE timers (
+	id_date int NOT NULL AUTO_INCREMENT,
+    id_user int NOT NULL,
+    date varchar(50) NOT NULL UNIQUE,
+    ini_m time NOT NULL,
+    fin_m time NOT NULL,
+    ini_a time NOT NULL,
+    fin_a time NOT NULL,
+    PRIMARY KEY (id_date)
+);
+
+
+INSERT INTO timers (id_user, date, ini_m, fin_m, ini_a,fin_a)
+VALUES ('2', '2022/09/15', '08:00' ,'13:00','15:00','17:00');
+
+select * from timers
+
+select * from timers where date like '%08%'
